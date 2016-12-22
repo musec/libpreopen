cc25de451063286979ed446f92c48103be09b8ae
Denoble <ujay2002@yahoo.com>
2016-12-21 23:07:11 -0330

    code review correction 1

--- lib/interceptAccess.c
+++ lib/interceptAccess.c
@@ -4,7 +4,6 @@
  *  Created on: Oct 15, 2016
  *      Author: ujay
  */
-#define _GNU_SOURCE
 #include<dlfcn.h>
 #include<stdio.h>
 #include<sys/stat.h>
@@ -14,12 +13,11 @@
 #include<sys/types.h>
 #include<dirent.h>
 #include<fcntl.h>
-#include"../include/cwrapHeader.h"
+#include"cwrapHeader.h"
 
-int (*real_faccessat)(int fd1,const char* pathname1,int mode,int flag)=NULL;
+int (*real_faccessat)(int fd1,const char* pathname1,int mode,int flag);
 int access(const char *pathname,int mode){
 	real_faccessat=dlsym(RTLD_NEXT,"faccessat");
-	printf("from my access() function \n");
 	return real_faccessat(AT_FDCWD,pathname,mode, AT_EACCESS);
 
 }

b18c6b0c52e6e1f5714cf9f2945917308bd18503
Jonathan Anderson <jonathan.anderson@ieee.org>
2016-12-30 00:57:51 -0330

    Add black-box unit test for `open(2)` wrapping.
    This test currently exposes a bug in the library (null dereference).

---
+++ test/open.c
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2016 Jonathan Anderson
+ * All rights reserved.
+ *
+ * This software was developed at Memorial University under the
+ * NSERC Discovery program (RGPIN-2015-06048).
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+/*
+ * RUN: %cc -c %cflags -D TEST_DATA_DIR="\"%p/Inputs\"" %s -o %t.o
+ * RUN: %cc %t.o %ldflags -o %t
+ * RUN: %t > %t.out
+ * RUN: %filecheck %t.out -input-file %s
+ */
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "cwrapHeader.h"
+
+
+int main(int argc, char *argv[])
+{
+	struct po_map *map = getMap();
+	
+	map = getMap();
+	map = po_preopen(map, TEST_DATA_DIR "/foo", O_RDONLY);
+	map = po_preopen(map, TEST_DATA_DIR "/baz/wibble", O_RDONLY);
+
+	// CHECK: Opening foo/bar/hi.txt...
+	printf("Opening foo/bar/hi.txt...\n");
+	int fd = open("foo/bar/hi.txt", O_RDONLY);
+
+	// CHECK: Got FD: 4
+	printf("Got FD: %d\n", fd);
+
+	// CHECK: Opening baz/wibble/bye.txt...
+	printf("Opening baz/wibble/bye.txt...\n");
+	fd = open("baz/wibble/bye.txt", O_RDONLY);
+
+	// CHECK: Got FD: 5
+	printf("Got FD: %d\n", fd);
+
+	return 0;
+}

1efa0a8f6f2b574692f6942a3fc084f55eff4ddc
Jonathan Anderson <jonathan.anderson@ieee.org>
2018-03-29 12:30:11 -0230

    Drop unneeded header imports.

--- lib/wrappers.c
+++ lib/wrappers.c
@@ -32,12 +32,9 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
-#include <dirent.h>
-#include <errno.h>
 #include <fcntl.h>
 #include <stdarg.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 
 #include <libpreopen.h>
